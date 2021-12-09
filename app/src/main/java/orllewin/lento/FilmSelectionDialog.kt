cc9f4b63ed37a9eec9e3344a0faae7397bb91270
ÖLAB <oppen@fastmail.se>
2021-12-09 21:45:36 +0000

    add Lento colour films

--- app/src/main/java/orllewin/lento/FilmSelectionDialog.kt
+++ app/src/main/java/orllewin/lento/FilmSelectionDialog.kt
@@ -22,6 +22,31 @@ class FilmSelectionDialog(val context: Context, val onFilmSelect: (resId: Int?, 
         val view: View = LayoutInflater.from(context).inflate(R.layout.dialog_film_selection, null, false)
         val binding = DialogFilmSelectionBinding.bind(view)
 
+        //Lento
+        val lentoLabels = context.resources.getStringArray(R.array.lento_film_labels)
+        val lentoResources = context.resources.getStringArray(R.array.lento_film_raw_resources)
+
+        lentoLabels.forEachIndexed { index, label ->
+            val chip = Chip(context)
+
+            chip.text = label
+
+            val rawId = lentoResources[index]
+            chip.tag = rawId
+            chip.isCheckable = true
+            chip.id = ViewCompat.generateViewId()
+            chip.setOnClickListener(this)
+            binding.flexBoxLento.addView(chip)
+        }
+
+        binding.flexBoxLento.children.iterator().forEach { chip ->
+            val lp = chip.layoutParams as FlexboxLayout.LayoutParams
+            lp.setMargins(0, 0, 8.toPixels(), 0)
+            chip.layoutParams = lp
+        }
+
+        //End of Lento
+
         //Colour
         val fileLabelsColour = context.resources.getStringArray(R.array.film_labels_colour)
         val fileResourcesColour = context.resources.getStringArray(R.array.film_raw_resources_colour)
